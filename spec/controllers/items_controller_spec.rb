0dec45172626645a1cae7edef296bdf1d2748c46
Kerry Buckley <kerryjbuckley@gmail.com>
2015-03-21 17:29:08 +0000

    Create an "items" variable
    Spec now fails "properly" (ie with a failed expectation rather than an
    undefined variable):
    
      expected: [#<RSpec::Mocks::Double:0x3fe4aedcab40 @name=:item>]
                  got: nil

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 
 describe ItemsController do
   describe "GET index" do
+    let(:items) { [double(:item)] }
+
     it "assigns all the items for the view" do
       get :index
       expect(assigns[:items]).to eq items
