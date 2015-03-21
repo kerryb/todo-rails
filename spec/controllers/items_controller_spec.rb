38373f50dee0051ca3c52fec187e30e2a2f79bc2
Kerry Buckley <kerryjbuckley@gmail.com>
2015-03-21 17:37:14 +0000

    Stub query on Item
    This doesn't change the failure message, but we now have everything in
    place for the behaviour we want to implement.

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -4,6 +4,10 @@ describe ItemsController do
   describe "GET index" do
     let(:items) { [double(:item)] }
 
+    before do
+      allow(Item).to receive(:all) { items }
+    end
+
     it "assigns all the items for the view" do
       get :index
       expect(assigns[:items]).to eq items
