0d0a1c9c00265a49022ac9277afc7c8e695ff1b2
Kerry Buckley <kerryjbuckley@gmail.com>
2015-03-21 11:59:59 +0000

    Add a migration to create the items table
        bin/rails g migration CreateItem name:string
        rake
    
    Now our first "Given" step succeeds!

---
+++ db/migrate/20150321115919_create_item.rb
@@ -0,0 +1,7 @@
+class CreateItem < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :name
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150321115919) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
