6af5281014a8e37db4f77285318425ff31d6eef1
Kerry Buckley <kerryjbuckley@gmail.com>
2015-03-22 11:12:28 +0000

    Failing spec for mark_done action marking item as done
    The initial failure message is warning us that we're stubbing a method
    on the Item class that doesn't exist yet (this is rspec's default
    behaviour when stubbing or mocking methods on real objects):
    
      Item(id: integer, name: string) does not implement: mark_done

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -35,8 +35,17 @@ describe ItemsController do
   end
 
   describe "POST mark_done" do
+    before do
+      allow(Item).to receive :mark_done
+    end
+
     def do_mark_done
       post :mark_done, id: "1"
+    end
+
+    it "marks the item as done" do
+      do_mark_done
+      expect(Item).to have_received(:mark_done).with "1"
     end
 
     it "redirects to the index" do
